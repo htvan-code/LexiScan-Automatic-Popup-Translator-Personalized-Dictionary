91c01c84765f9fc419ba6a0d337da5562b080abf
htvan-code <htvan.se@gmail.com>
2025-12-15 21:04:41 +0700

    create folder

--- ScreenTranslator/ScreenTranslator/Services/ClipboardHookService.cs
+++ ScreenTranslator/ScreenTranslator/Services/ClipboardHookService.cs
@@ -10,8 +10,6 @@ namespace ScreenTranslator
         public event Action<string>? OnTextCaptured;
         public event Action<string>? OnError;
 
-        // --- CÁC HẰNG SỐ MODIFIER ---
-        // Person 3 sẽ gửi số này vào cho bạn
         public const int MOD_NONE = 0x0000;
         public const int MOD_ALT = 0x0001;
         public const int MOD_CONTROL = 0x0002;
@@ -27,35 +25,28 @@ namespace ScreenTranslator
         [DllImport("user32.dll")]
         private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
 
-        // Lưu giữ Handle cửa sổ để dùng lại khi đổi Hotkey
         private IntPtr _windowHandle;
         private bool _isRegistered = false;
 
-        // --- HÀM 1: ĐĂNG KÝ LẦN ĐẦU ---
         public void Register(IntPtr windowHandle, int modifier, int key)
         {
             _windowHandle = windowHandle;
             UpdateHotkey(modifier, key);
         }
 
-        // --- HÀM 2: ĐỔI HOTKEY (QUAN TRỌNG NHẤT) ---
         public void UpdateHotkey(int modifier, int key)
         {
-            // 1. Nếu đang có hotkey cũ thì hủy đi
             if (_isRegistered)
             {
                 UnregisterHotKey(_windowHandle, HOTKEY_ID);
                 _isRegistered = false;
             }
 
-            // 2. Đăng ký hotkey mới
             bool success = RegisterHotKey(_windowHandle, HOTKEY_ID, modifier, key);
 
             if (success)
             {
                 _isRegistered = true;
-                // (Tùy chọn) Báo ra ngoài là đổi thành công
-                // OnError?.Invoke($"Đã đổi Hotkey sang: Mod={modifier}, Key={key}");
             }
             else
             {
