5fd69f970fbce23d9de2724d41fe7b19a7aad1fb
qthinh1006 <qthinh1006@gmail.com>
2025-12-16 10:44:33 +0700

    hihihi

--- LexiScan/LexiScanData/Models/Words.cs
+++ LexiScan/LexiScanData/Models/Words.cs
@@ -1,19 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-namespace LexiScanData.Models // <--- Kiểm tra kỹ dòng này
+
+namespace LexiScanData.Models // <--- Quan trọng: Namespace phải đúng
 {
-    // Phải có chữ "public" ở đầu thì Project khác mới thấy được
-    public class Word
+    public class Word // <--- Phải là "public" và tên là "Word"
     {
         public int WordId { get; set; }
         public string Text { get; set; }
         public string Meaning { get; set; }
         public string Pronunciation { get; set; }
         public string WordType { get; set; }
-        public DateTime Timestamp { get; set; } // Thêm cái này để lưu lịch sử
+        public DateTime Timestamp { get; set; }
     }
 }
-
